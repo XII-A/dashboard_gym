4bc6956910279771ed86905c99415c844f835005
Mahmoud Hilani <mahmoudhilani18@gmail.com>
2024-01-09 17:03:59 +0300

    feat: progress card prototype

--- dashboard/src/app/MainPage/Progress/page.js
+++ dashboard/src/app/MainPage/Progress/page.js
@@ -1,7 +1,19 @@
 import React from "react";
+import ProgressBarCard from "@/components/ProgressBar";
 
 const page = () => {
-  return <div>Progress</div>;
+  return (
+    <div className="bg-bgColor-primary h-full p-5">
+      <ProgressBarCard
+        workoutCurrent={60}
+        workoutGoal={120}
+        stepsCurrent={1000}
+        stepsGoal={10000}
+        caloriesCurrent={2000}
+        caloriesGoal={2000}
+      />
+    </div>
+  );
 };
 
 export default page;
